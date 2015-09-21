d4d4d599020ef2948423487bdbfe631bfdec3938
SantiagoBoffa <santiagoboffa@outlook.com>
2015-09-21 18:38:22 -0300

    Factura v 2.0

--- client/templates/administradores/Factura/administradores_facturas.js
+++ client/templates/administradores/Factura/administradores_facturas.js
@@ -1,33 +1,32 @@
-Meteor.users.initEasySearch('profile.nombre');
+var selecte_id;
+selecteDep = new Tracker.Dependency();
 
 Template.administradoresFacturas.helpers({
-   users: function() {return Meteor.users.find();},
-   proyectos: function() {return Proyectos.find();}
-  
-});
+   users: function() {
+    return Meteor.users.find();
+   },
+   proyectos: function() {
+    selecteDep.depend();
+    return Proyectos.find({idcliente:selecte_id});
+  }
+ });
 
-Template.administradoresFacturas.events({
 
-  "keydown .buscar" : function(e,t){
-
-if ($(".buscar").val() == "") {
-  $(".defaulttable").show();
-}
-else{
-   $(".defaulttable").hide();
-}
-  },
-
-  'click .responder': function (e,t) {
-    Router.go('facturasPost',{_id:$(e.target).closest('.responder').data('id')})
-  },
+  Template.administradoresFacturas.events({
 
   'click #popup': function () {
     $('#myModal').on('shown.bs.modal', function () {
         $('#myInput').focus()
     })
-}
-
-
-})
-
+},
+'click .filtrar': function (e, t) {
+     selecte_id =  $(e.target).closest('.filtrar').data('id');
+     selecteDep.changed();
+    },
+     "click .agregar": function (e, t) {
+      Router.go('administradoresFacturasAgregar',{_id:$(e.target).closest('.agregar').data('id')});
+      $('#myModal').on('hidden', function(){
+        $(this).data('modal', null);
+      });
+    }
+});
